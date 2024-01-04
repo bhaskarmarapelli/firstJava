efadd3c423b1608b62b173ff48a390d529cfcb76
Dr. BHASKAR MARAPELLI <34471404+bhaskarmarapelli@users.noreply.github.com>
2024-01-04 13:01:44 +0530

    Update MyClass.java

--- MyClass.java
+++ MyClass.java
@@ -2,10 +2,10 @@ class MyClass {
   public static void main(String[] args) {
     int x, y, sum;
     
-    x = 100
+    x = 100;
 
     
-    y = 200
+    y = 200;
 
     sum = x + y;  // Calculate the sum of x + y
     System.out.println("Sum is: " + sum); // Print the sum
