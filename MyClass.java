98487f2b31015475911bb857780a3fb81835e470
Dr. BHASKAR MARAPELLI <34471404+bhaskarmarapelli@users.noreply.github.com>
2024-01-04 13:03:19 +0530

    Update MyClass.java

--- MyClass.java
+++ MyClass.java
@@ -1,13 +1,16 @@
+import java.util.Scanner; // Import the Scanner class
+
 class MyClass {
   public static void main(String[] args) {
     int x, y, sum;
-    
-    x = 100;
+    Scanner myObj = new Scanner(System.in); // Create a Scanner object
+    System.out.println("Type a number:");
+    x = myObj.nextInt(); // Read user input
 
-    
-    y = 200;
+    System.out.println("Type another number:");
+    y = myObj.nextInt(); // Read user input
 
     sum = x + y;  // Calculate the sum of x + y
     System.out.println("Sum is: " + sum); // Print the sum
   }
-}
+} 
